c0790e88ab52c1cede346e1d4f6042feed256f0b
Kevin Barasa <kevin.barasa001@gmail.com>
2017-03-07 14:55:31 +0300

    ToH Basic implementation

---
+++ tower_of_hanoi_basic.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    static void solve(int n, String start, String auxiliary, String end) {
+       if (n == 1) {
+           System.out.println(start + " -> " + end);
+       } else {
+           solve(n - 1, start, end, auxiliary);
+           System.out.println(start + " -> " + end);
+           solve(n - 1, auxiliary, start, end);
+       }
+   }
+    
+    
+    public static void main(String[] args) {
+       solve(3,"A", "B", "C");
+    }
+}
